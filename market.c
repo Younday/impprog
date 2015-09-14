2b4e50dd44f299cf230daa643715b90dc85c0bca
Younes Moustaghfir <Younesmous@gmail.com>
2015-09-14 12:48:16 +0200

    Almost working

--- market.c
+++ market.c
@@ -12,7 +12,7 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	int mbought, op, gp, mp, total, totalp;
+	int obought, gbought, mbought, op, gp, mp, total, totalp;
 	
 	printf("price of an orange: ");
 	scanf("%d", &op);
@@ -21,33 +21,20 @@ int main(int argc, char *argv[]) {
 	printf("price of a melon: ");
 	scanf("%d", &mp);
 	
-	for(int obought = 0; obought <= 100; obought++) {
-		for(int gbought = 0; gbought <= 100; gbought++) {
+	for(obought = 0; obought <= 100; obought++) {
+		for(gbought = 0; gbought <= 100; gbought++) {
 			mbought = 100 - obought - gbought;
 			total = obought + gbought + mbought;
 			totalp = (gbought*gp) + (obought*op) + (mbought*mp);
-			if(totalp == 10000 && total == 100 && obought <= 1) {
+			if(totalp == 10000 && total == 100) {
+				printf("%d oranges, %d grapefruits, %d melons\n", obought, gbought, mbought);
+			}
+			else if(totalp == 10000 && total == 100 && obought == 1) {
 				printf("%d orange, %d grapefruits, %d melons\n", obought, gbought, mbought);
 			}
-			else if(totalp == 10000 && total == 100 && obought > 1) {
-				printf("%d oranges, %d grapefruits, %d melons\n", obought, gbought, mbought);
+			else if(totalp == 10000 && total == 100 && mbought == 1) {
+				printf("%d orange, %d grapefruits, %d melon\n", obought, gbought, mbought);
 			}
-			else if(totalp == 10000 && total == 100 && gbought <= 1) {
-				printf("%d oranges, %d grapefruit, %d melons\n", obought, gbought, mbought);
-			}
-			else if(totalp == 10000 && total == 100 && gbought > 1)	{
-				printf("%d oranges, %d grapefruits, %d melons\n", obought, gbought, mbought);
-			}
-			else if(totalp == 10000 && total == 100 && mbought <= 1) {
-				printf("%d oranges, %d grapefruits, %d melon\n", obought, gbought, mbought);
-			}
-			else if(totalp == 10000 && total == 100 && mbought > 1){
-				printf("%d oranges, %d grapefruits, %d melons\n", obought, gbought, mbought);
-				}
 			}
 		}
 	}
-
-		
-
-
