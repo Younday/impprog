c967a7b385e064af0ffeb2f316703db860e433c4
Younes Moustaghfir <Younesmous@gmail.com>
2015-10-01 18:52:48 +0200

    Working version of disc assignment

--- discs/discs.c
+++ discs/discs.c
@@ -24,7 +24,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-int sz;
+#include <math.h>
 
 void *safeMalloc(int size) {
   void *ptr = malloc(size);
@@ -35,23 +35,59 @@ void *safeMalloc(int size) {
   return ptr;
 }
 
-int **makeIntArray2D(int width, int height) {
+int **makeDiscs(int sz) {
   int row, **arr;
-  arr = safeMalloc(height*sizeof(int *));
-  for (row=0; row<height; row++) {
-    arr[row] = safeMalloc(width*sizeof(int));
+  arr = safeMalloc(sz*sizeof(int *));
+  for (row=0; row<sz; row++) {
+    arr[row] = safeMalloc(4*sizeof(int));
   }
   return arr;
 }
 
-int *readDiscs() {
-	scanf("%d\n", &sz);
-	int discs
+int intersect(int disc1, int disc2, int **discs) {
+	int sxD, syD, count = 0;
+	int xD = discs[disc1][0] - discs[disc2][0];
+	int yD = discs[disc1][1] - discs[disc2][1];
+	sxD = xD * xD;
+	syD = yD * yD;
+	if(sxD + syD <= (discs[disc1][2]+discs[disc2][2])*(discs[disc1][2]+discs[disc2][2])) {
+		count = 1;
 	}
+	return count;
+}
+
+int countIntersect(int sz, int **discs) {
+	int count = 0;
+	for(int i = 0; i < (sz-1); i++) {
+		for(int j = i + 1; j < sz; j++) {
+			if(intersect(i, j, discs)) {
+				discs[i][3] = 1;
+				discs[j][3] = 1;
+			}
+		}
+	}
+	for(int i = 0; i < sz; i++) {
+		if(discs[i][3]) {
+			count++;
+		}
+	}
+	return count;
+}
+
+
 
 int main(int argc, char **argv)
 {
-	
+	int count;
+	int sz;
+	int **discs;
+	scanf("%d\n", &sz);
+	discs = makeDiscs(sz);
+	for(int i = 0; i < sz; i++) {
+		scanf("%d %d %d\n", &discs[i][0], &discs[i][1], &discs[i][2]);
+	}
+	count = countIntersect(sz, discs);
+	printf("number of discs: %d\n", sz);
+	printf("number of non-overlapping discs: %d\n", sz-count);
 	return 0;
 }
-
